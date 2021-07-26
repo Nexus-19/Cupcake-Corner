1b6e82768c6645751ef8c88b7fb153e5e9283d67
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-07-26 09:45:33 +0530

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Soumyattam Dey on 26/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
